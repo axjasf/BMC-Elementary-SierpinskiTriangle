55e1daf5a0d6833a29e6be12ab80991bcbbf52c8
AJ <38798090+axjasf@users.noreply.github.com>
2023-11-12 11:24:45 -0800

    feat: Initial working code

--- beautifulchaos.py
+++ beautifulchaos.py
@@ -3,7 +3,33 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import random
 
-# ... [previous functions remain the same] ...
+# Function to plot an equilateral triangle
+def plot_equilateral_triangle(ax, start_point, side_length):
+    x_start, y_start = start_point
+    x_middle = x_start + side_length / 2
+    y_middle = y_start + (np.sqrt(3) / 2) * side_length
+    x_end = x_start + side_length
+    triangle_points = np.array([[x_start, y_start], [x_middle, y_middle], [x_end, y_start], [x_start, y_start]])
+    ax.plot(triangle_points[:, 0], triangle_points[:, 1], '-o')
+    return [(x_start, y_start), (x_middle, y_middle), (x_end, y_start)]
+
+# Function to move towards a point
+def move_towards_point(old_point, target_point):
+    new_x = (old_point[0] + target_point[0]) / 2
+    new_y = (old_point[1] + target_point[1]) / 2
+    return new_x, new_y
+
+# Function for the dice roll and move
+def roll_dice_and_move(start_point, triangle_points):
+    roll = random.randint(1, 6)
+    if roll in [1, 2]:
+        target_point = triangle_points[0]
+    elif roll in [3, 4]:
+        target_point = triangle_points[1]
+    else:
+        target_point = triangle_points[2]
+    new_point = move_towards_point(start_point, target_point)
+    return new_point
 
 # Adjusted Animation function
 def update(frame):
